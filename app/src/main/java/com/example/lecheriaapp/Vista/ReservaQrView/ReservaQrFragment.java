eee68c5ae5e19a6e054e4f742b8e464c1c3a67df
Sergio Alexander Huayllas Tirado <aogiri98a@gmail.com>
2023-11-16 17:45:31 -0500

    Finalizado qr de reserva

--- app/src/main/java/com/example/lecheriaapp/Vista/ReservaQrView/ReservaQrFragment.java
+++ app/src/main/java/com/example/lecheriaapp/Vista/ReservaQrView/ReservaQrFragment.java
@@ -1,49 +1,40 @@
 package com.example.lecheriaapp.Vista.ReservaQrView;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 import com.example.lecheriaapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReservaQrFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ReservaQrFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_QR_CODE_URL = "qr_code_url";
+    private static final String ARG_RESERVA_CODIGO = "reserva_codigo";
+    private static final String ARG_NOMBRE_USUARIO = "nombre_usuario";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String qrCodeUrl;
+    private String reservaCodigo;
+    private String nombreUsuario;
 
     public ReservaQrFragment() {
-        // Required empty public constructor
+        // Constructor vacío requerido por Fragment
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReservaQrFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReservaQrFragment newInstance(String param1, String param2) {
+    public static ReservaQrFragment newInstance(String qrCodeUrl, String reservaCodigo, String nombreUsuario) {
         ReservaQrFragment fragment = new ReservaQrFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_QR_CODE_URL, qrCodeUrl);
+        args.putString(ARG_RESERVA_CODIGO, reservaCodigo);
+        args.putString(ARG_NOMBRE_USUARIO, nombreUsuario);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,15 +43,38 @@ public class ReservaQrFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            qrCodeUrl = getArguments().getString(ARG_QR_CODE_URL);
+            reservaCodigo = getArguments().getString(ARG_RESERVA_CODIGO);
+            nombreUsuario = getArguments().getString(ARG_NOMBRE_USUARIO);
+            Log.d("QR", "URL del código QR: " + qrCodeUrl);
+            Log.d("QR", "Código de Reserva: " + reservaCodigo);
+            Log.d("QR", "Nombre de Usuario: " + nombreUsuario);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reserva_qr, container, false);
+        View view = inflater.inflate(R.layout.fragment_reserva_qr, container, false);
+        ImageView qrCodeImageView = view.findViewById(R.id.qrCodeImageView);
+        TextView codigoReservaTextView = view.findViewById(R.id.codigoReservaTextView);
+        TextView nombreUsuarioTextView = view.findViewById(R.id.nombreUsuarioTextView);
+
+        // Verificar si la URL es válida antes de cargarla
+        if (!TextUtils.isEmpty(qrCodeUrl)) {
+            // Cargar el código QR utilizando Glide (puedes usar Picasso también)
+            Glide.with(this)
+                    .load(qrCodeUrl)
+                    .into(qrCodeImageView);
+
+            // Mostrar el código de reserva y el nombre del usuario
+            codigoReservaTextView.setText("Código de Reserva: " + reservaCodigo);
+            nombreUsuarioTextView.setText("Nombre de Usuario: " + nombreUsuario);
+        } else {
+            // Manejar el caso de URL nula o vacía
+            Toast.makeText(requireContext(), "URL del código QR no válida", Toast.LENGTH_SHORT).show();
+        }
+
+        return view;
     }
 }

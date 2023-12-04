80f0ae7d13cec04b3f52b7bd84e69839151c424f
Sergio Alexander Huayllas Tirado <aogiri98a@gmail.com>
2023-12-04 11:56:07 -0500

    Correcion de dashboard

--- app/src/main/java/com/example/lecheriaapp/Presentador/GestionReservasPresenter/PresentadorGestionReservas.java
+++ app/src/main/java/com/example/lecheriaapp/Presentador/GestionReservasPresenter/PresentadorGestionReservas.java
@@ -1,5 +1,4 @@
 package com.example.lecheriaapp.Presentador.GestionReservasPresenter;
-
 import androidx.annotation.NonNull;
 
 import com.example.lecheriaapp.Modelo.ProductoModel;
@@ -13,19 +12,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
-
+import java.util.Map;
 
 public class PresentadorGestionReservas {
     private GestionReservasView view;
@@ -35,6 +31,53 @@ public class PresentadorGestionReservas {
     }
 
     public void obtenerReservasDesdeFirebase() {
+        obtenerRolUsuario();
+    }
+
+    // Método para obtener el key identificador del usuario
+    public void obtenerKeyUsuario() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            view.mostrarKeyUsuario(uid);
+        }
+    }
+
+    // Método para obtener el rol del usuario
+    public void obtenerRolUsuario() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("Usuarios").child(uid);
+            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    UserModel userModel = dataSnapshot.getValue(UserModel.class);
+                    if (userModel != null) {
+                        String userRol = userModel.getRol();
+
+                        // Verificar el rol del usuario antes de mostrar las reservas
+                        if ("ADMIN".equals(userRol)) {
+                            // Si el rol es ADMIN, mostrar todas las reservas
+                            obtenerTodasLasReservas();
+                        } else {
+                            // Si no es ADMIN, mostrar las reservas del usuario actual
+                            obtenerReservasUsuario(uid);
+                        }
+
+                        view.mostrarRolUsuario(userRol);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    // Manejar el error, si es necesario
+                }
+            });
+        }
+    }
+
+    private void obtenerTodasLasReservas() {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("reservas");
 
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -95,42 +138,76 @@ public class PresentadorGestionReservas {
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                view.mostrarMensaje("Error al obtener las reservas");
+                view.mostrarMensaje("Error al obtener todas las reservas");
             }
         });
     }
 
-    // Método para obtener el key identificador del usuario
-    public void obtenerKeyUsuario() {
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null) {
-            String uid = currentUser.getUid();
-            view.mostrarKeyUsuario(uid);
-        }
-    }
+    private void obtenerReservasUsuario(String usuarioId) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("reservas");
 
-    // Método para obtener el rol del usuario
-    public void obtenerRolUsuario() {
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null) {
-            String uid = currentUser.getUid();
-            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("Usuarios").child(uid);
-            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    UserModel userModel = dataSnapshot.getValue(UserModel.class);
-                    if (userModel != null) {
-                        String userRol = userModel.getRol();
-                        view.mostrarRolUsuario(userRol);
+        databaseReference.orderByChild("usuarioId").equalTo(usuarioId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        List<ReservaModel> reservas = new ArrayList<>();
+
+                        // Formato de fecha actual en tus datos
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault());
+
+                        for (DataSnapshot reservaSnapshot : dataSnapshot.getChildren()) {
+                            DataSnapshot productosSnapshot = reservaSnapshot.child("productos");
+                            Map<String, ProductoModel> productosMap = new HashMap<>();
+
+                            for (DataSnapshot productoSnapshot : productosSnapshot.getChildren()) {
+                                ProductoModel producto = productoSnapshot.getValue(ProductoModel.class);
+                                productosMap.put(productoSnapshot.getKey(), producto);
+                            }
+
+                            try {
+                                // Convertir la fecha a objeto Date
+                                Date fechaDate = dateFormat.parse(reservaSnapshot.child("fecha").getValue(String.class));
+
+                                ReservaModel reserva = new ReservaModel(
+                                        reservaSnapshot.getKey(),
+                                        reservaSnapshot.child("estado").getValue(String.class),
+                                        dateFormat.format(fechaDate), // Guardar la fecha formateada si es necesario
+                                        productosMap,
+                                        reservaSnapshot.child("subtotal").getValue(Integer.class),
+                                        reservaSnapshot.child("total").getValue(Integer.class),
+                                        reservaSnapshot.child("usuarioId").getValue(String.class),
+                                        reservaSnapshot.child("qr").getValue(String.class)
+                                );
+
+                                reservas.add(reserva);
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        // Ordenar la lista de reservas por fecha
+                        Collections.sort(reservas, new Comparator<ReservaModel>() {
+                            @Override
+                            public int compare(ReservaModel r1, ReservaModel r2) {
+                                try {
+                                    Date date1 = dateFormat.parse(r1.getFecha());
+                                    Date date2 = dateFormat.parse(r2.getFecha());
+                                    return date2.compareTo(date1); // Ordenar de forma descendente
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                    return 0;
+                                }
+                            }
+                        });
+
+                        view.mostrarReservas(reservas);
                     }
-                }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    // Manejar el error, si es necesario
-                }
-            });
-        }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        view.mostrarMensaje("Error al obtener las reservas del usuario");
+                    }
+                });
     }
 
     public interface GestionReservasView {
